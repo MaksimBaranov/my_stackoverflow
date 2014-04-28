efbe195693b2267fe00cccd25b7cac8530266410
Maksim Baranov <openok1989@mail.ru>
2014-04-28 23:39:54 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyStackoverflow::Application.config.secret_key_base = '8c9ddc53f6b2a50b812569c3807f9cc6cbcd33e7dc448341fb56c01dc1537327edc7265854eacf7b69a2de0e5f356d94c66ba925a1325d4ebac9b94f694da085'
