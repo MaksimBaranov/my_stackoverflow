5d7540498951773257f054a80af493ea40095130
Maksim Baranov <openok1989@mail.ru>
2014-04-29 21:13:27 +0300

    added validation of length with passing test

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   validates :title, :body, presence: true
+  validates :title, length: { maximum: 50 }
+  validates :body, length: { maximum: 150 }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe Question do
   it { should validate_presence_of :title}
   it { should validate_presence_of :body}
+  it { should ensure_length_of(:title).is_at_most(50)}
+  it { should ensure_length_of(:body).is_at_most(150)}
 end
