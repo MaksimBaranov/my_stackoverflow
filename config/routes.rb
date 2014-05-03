26d2905e97b9002a417bb6ac7606fb51bf29dd3a
Maksim Baranov <openok1989@mail.ru>
2014-05-03 15:17:26 +0300

    Gem devise was setted and user model was created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MyStackoverflow::Application.routes.draw do
+  devise_for :users
   root 'questions#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
