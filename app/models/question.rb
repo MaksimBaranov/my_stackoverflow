a89a8e25a82a0485f8ae984e02279b4495d51481
Maksim Baranov <openok1989@mail.ru>
2014-05-04 15:52:04 +0300

    Associations user-question was added and all tests were passed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+  belongs_to :user
   validates :title, :body, presence: true
   validates :title, length: { maximum: 50 }
   validates :body, length: { maximum: 300 }
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,5 @@ describe Question do
   it { should validate_presence_of :body}
   it { should ensure_length_of(:title).is_at_most(50)}
   it { should ensure_length_of(:body).is_at_most(300)}
+  it { should belong_to(:user) }
 end
