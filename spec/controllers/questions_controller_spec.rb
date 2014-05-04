b4ab3b881b504b56ca874ab2f6504d97dd4fcf39
Maksim Baranov <openok1989@mail.ru>
2014-05-04 15:06:56 +0300

    Testing index action Question controller.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe QuestionsController do
+  describe "GET #index" do
+    let(:questions) { create_list(:question, 2) }
 
+    before { get :index }
+
+    it 'fill in an array of all questions'  do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
 end
