67658a70fb21754f8219abcfefc323ad719781a9
Maksim Baranov <openok1989@mail.ru>
2014-05-07 07:14:52 +0300

    Features create and show answer and question were added.

---
+++ spec/features/create_answer_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature "Create answer", %q{
+  In order to answer  the question
+  As an authenticated user
+  I want to be able to write the answer
+  } do
+end
---
+++ spec/features/show_question_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature "Show question", %q{
+  In order to look quts of question
+  As an random user
+  I want to be able to get access to the question
+  } do
+end
