92792379236c38bbc879f8e7dafa088a63294fb7
Maksim Baranov <openok1989@mail.ru>
2014-05-10 17:47:58 +0300

    Refactoring questions_controller_spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe QuestionsController do
 
-  describe "GET #index" do
+  describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
     before { get :index }
@@ -16,9 +16,9 @@ describe QuestionsController do
     end
   end
 
-  describe "GET #new" do
+  describe 'GET #new' do
 
-    context "user is sign in" do
+    context 'user is sign in' do
       login_user
       before(:each) do
         get :new
@@ -33,63 +33,67 @@ describe QuestionsController do
       end
     end
 
-    context "user is a quest" do
-      it "redirect to sign in page" do
+    context 'user is a quest' do
+      it 'redirect to sign in page' do
         get :new
         expect(response).to redirect_to new_user_session_path
       end
     end
   end
 
-  describe "GET #show" do
-    shared_examples "checks @question and renders template show" do
+  describe 'GET #show' do
+    shared_examples 'checks @question and renders template show' do
       let(:question) { create(:question) }
-      before(:each) {get :show, id: question}
+      before(:each) { get :show, id: question }
 
-      it "checks @question" do
+      it 'checks @question' do
         expect(assigns(:question)).to eq question
       end
 
-      it "renders template new" do
+      it 'renders template new' do
         expect(response).to render_template :show
       end
     end
 
-    context "user is a quest" do
-      it_should_behave_like "checks @question and renders template show"
+    context 'user is a quest' do
+      it_should_behave_like 'checks @question and renders template show'
     end
 
-    context "user is sign in"do
+    context 'user is sign in' do
       login_user
-      it_should_behave_like "checks @question and renders template show"
+      it_should_behave_like 'checks @question and renders template show'
     end
   end
 
-  describe "POST #create" do
+  describe 'POST #create' do
     context 'user is sign in' do
       login_user
+
+      def request(type_data)
+        post :create, question: attributes_for(type_data)
+      end
+
       context 'with valid attributes' do
         it 'saves the new question in the database' do
-          expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+          expect { request(:question) }.to change(Question, :count).by(1)
         end
 
         it 'redirects to show view' do
-          post :create, question: attributes_for(:question)
+          request(:question)
           expect(response).to redirect_to question_path(assigns(:question))
         end
 
-        it "saves the new question withattribute user_id" do
-          post :create, question: attributes_for(:question)
-          expect(assigns[:question][:user_id]).to eq @user.id
+        it 'saves the new question withattribute user_id' do
+          expect { request(:question) }.to change(@user.questions, :count)
         end
       end
 
       context 'with invalid attributes' do
         it 'does not save question to the database' do
-          expect{ post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+          expect { request(:invalid_question) }.to_not change(Question, :count)
         end
         it 're-renders new view' do
-          post :create, question: attributes_for(:invalid_question)
+          request(:invalid_question)
           expect(response).to render_template :new
         end
       end
