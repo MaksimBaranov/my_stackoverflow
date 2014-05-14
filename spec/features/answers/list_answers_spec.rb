9dbc707e40abc154c2d8c483426da627c3d2a76a
Maksim Baranov <openok1989@mail.ru>
2014-05-14 23:00:35 +0300

    Acceptance-test for list answers is green.

--- spec/features/answers/list_answers_spec.rb
+++ spec/features/answers/list_answers_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
-feature '' do
+feature 'Look list answers', %q(
+  In order to look answers which related to question
+  As an random user
+  I want to be able to see answers
+) do
 
+  let(:question) { create(:question) }
+  let(:answers) { create_list(:answer,10) }
+
+  scenario 'Random user see list of answers to current question.' do
+    question.answers << answers
+    visit question_path(question)
+    expect(page).to have_content answers.first.text
+    expect(page).to have_content answers.last.text
+  end
 end
