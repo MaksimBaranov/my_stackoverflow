4445ae52ed9a2539c4ab20df7143421cc3a0f62b
Maksim Baranov <openok1989@mail.ru>
2014-05-20 19:17:34 +0300

    I added authority for edition comments, all aproriate tests are green.

--- spec/features/comments/edit_comment_spec.rb
+++ spec/features/comments/edit_comment_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Edit comment', %q(
-  In order to edit my comment to question/answer
+  In order to edit my own comment to question/answer
   As an authenticated user
   I want to be able to edit comment
 ) do
@@ -12,45 +12,63 @@ feature 'Edit comment', %q(
   given!(:comment1) { create(:comment) }
   given!(:comment2) { create(:comment) }
 
-  def init_question_answer_comment
+  def question_and_answer_have_comments
     answer.question = question
     answer.save
     question.comments << comment1
     answer.comments << comment2
   end
 
-  scenario 'Authenticated user edit comment for answer' do
-    init_question_answer_comment
-
+  def auth_user_is_author_of_question_and_answer
+    user.questions << question
+    user.answers << answer
+    user.comments << [comment1, comment2]
     new_user_session
+  end
+
+  scenario 'Authenticated user see edit-button for answer and question.' do
+    question_and_answer_have_comments
+    auth_user_is_author_of_question_and_answer
+    visit question_path(question)
+
+    within ".list-question-comments #comment-#{comment1.id}" do
+      expect(page).to have_content 'Improve Comment'
+    end
+
+    within "#answer-#{answer.id} #comment-#{comment2.id}" do
+      expect(page).to have_content 'Improve Comment'
+    end
+  end
+
+  scenario 'Authenticated user edit comment for answer' do
+    question_and_answer_have_comments
+    auth_user_is_author_of_question_and_answer
     visit question_path(question)
     within "#answer-#{answer.id} #comment-#{comment2.id}" do
       click_on('Improve Comment')
     end
     fill_in 'Text', with: comment2.text
     click_on 'Edit Comment'
+
     expect(page).to have_content %q(Your comment has been succesfully updated.)
   end
 
   scenario 'Authenticated user edit comment for question' do
-    init_question_answer_comment
-
-    new_user_session
+    question_and_answer_have_comments
+    auth_user_is_author_of_question_and_answer
     visit question_path(question)
     within ".list-question-comments #comment-#{comment1.id}" do
       click_on('Improve Comment')
     end
     fill_in 'Text', with: comment2.text
     click_on 'Edit Comment'
+
     expect(page).to have_content %q(Your comment has been succesfully updated.)
   end
 
-  scenario 'Non-authenticated user try to edit comment' do
-    init_question_answer_comment
-    visit question_path(question)
-    within "#answer-#{answer.id} #comment-#{comment2.id}" do
-      click_on('Improve Comment')
-    end
+  scenario 'Non-authenticated user try to edit comment from address bar.' do
+    question_and_answer_have_comments
+    visit edit_comment_path(comment1 ||= comment2)
 
     expect(page).to have_content %q(You need to sign in
     or sign up before continuing.)
