26a3d02b1c58da27d6e7bd1f72de4e70f4c0ad7f
Maksim Baranov <openok1989@mail.ru>
2014-05-25 00:16:25 +0300

    Remove redundant and no actual migration.

--- db/migrate/20140517185946_add_vote_id_to_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddVoteIdToQuestions < ActiveRecord::Migration
-  def change
-    add_column :questions, :vote_id, :integer
-  end
-end
--- db/migrate/20140517185957_add_vote_id_to_answers.rb
+++
@@ -1,5 +0,0 @@
-class AddVoteIdToAnswers < ActiveRecord::Migration
-  def change
-    add_column :answers, :vote_id, :integer
-  end
-end
