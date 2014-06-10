1f80d6ea8ffca5e5d1b70e610c1e46eba6aed344
Maksim Baranov <openok1989@mail.ru>
2014-06-10 15:11:22 +0300

    tests for model are green

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,3 +2,5 @@ class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :questions, through: :taggings
 end
+
+
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -13,4 +13,52 @@ describe Question do
   it { should validate_presence_of :body }
   it { should ensure_length_of(:title).is_at_least(10).is_at_most(100) }
   it { should ensure_length_of(:body).is_at_least(50).is_at_most(600) }
+
+  let(:questions) { create_list(:question, 5) }
+  let(:question) { create(:question) }
+  let(:tag1) { create(:tag, name: 'Ruby') }
+  let(:tag2) { create(:tag, name: 'Rails') }
+  let(:tag3) { create(:tag, name: 'Ajax') }
+
+  context '::with_tag' do
+    it 'returns array of questions objects' do
+      questions.each do |q|
+        q.tags << tag1
+      end
+      questions_tag = Question.with_tag('Ruby')
+
+      expect(questions_tag).to match_array questions
+    end
+  end
+
+  context '#tags_list' do
+    it 'returns array of tag names' do
+      question.tags << [tag1, tag2, tag3]
+      list = question.tags_list
+
+      expect(list).to match_array [ tag1.name, tag2.name, tag3.name, question.tag_names ]
+    end
+  end
+
+
+  describe "#save_tags" do
+
+    context "when tag_names've existed already" do
+      it 'returns record with current name' do
+        tag1
+        new_question = create(:question, tag_names: 'Ruby')
+        expect{ new_question.save_tags }.to_not  change(Tag, :count)
+      end
+    end
+
+    # context 'when tag_names is a new' do
+    #   it 'save new record to DB' do
+    #     tag1
+    #     new_question = create(:question, tag_names: 'Hulk')
+
+
+    #     expect{  }.to  change(Tag, :count).by(1)
+    #   end
+    # end
+  end
 end
