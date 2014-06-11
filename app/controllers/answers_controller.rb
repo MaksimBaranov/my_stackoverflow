3643302b55573a6890cff01a33572c358635e066
Maksim Baranov <openok1989@mail.ru>
2014-06-11 13:26:21 +0300

    Answer controller IR is completed.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,83 +1,28 @@
-class AnswersController < ApplicationController
+class AnswersController < InheritedResources::Base
   before_filter :authenticate_user!
-  before_filter :load_question
-  before_filter :set_answer, :check_authority,  only: [:edit, :update]
-
-  def create
-    @answer = @question.answers.build(answer_params)
-    current_user.answers << @answer
-    respond_to do |format|
-      if  @answer.save
-        format.html {  redirect_to @question, notice: 'Your answer has been successfully created.' }
-        format.js do
-          # PrivatePub.publish_to "/questions/#{@question.id}/answers", answer: @answer.to_json(:include => [:vote, :comments, :attachments])
-          # render nothing: true
-        end
-        format.json { render json: @answer.to_json(:include => [:vote, :comments, :attachments]) }
-      else
-        format.html do
-          flash[:alert] = 'Your answer hasn`t been created. Try again.'
-          redirect_to @question
-        end
-        format.js
-        format.json { render json: @answer.errors.full_messages, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @question, notice: 'Answer has been successfully updated.' }
-        format.js
-      else
-        format.html do
-          flash[:alert] = 'Answer hasn`t been updated. Try again.'
-          render :edit
-        end
-        format.js
-      end
-    end
-  end
-
-  def destroy
-    @answer = Answer.find(params[:id])
-    @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to question_path(@question), notice: 'Your answer has been removed.' }
-      format.js
-    end
-  end
+  before_action :build_answer, only: :create
+  respond_to :js, :html
+  belongs_to :question
 
   def best
-    @answer = Answer.find(params[:id])
-    if @question.user == current_user
-      @answer.update_attributes(best: true)
-      redirect_to @question, notice: 'Your has been made answer best'
+    if parent.user == current_user
+      resource.update_attributes(best: true)
+      redirect_to parent, notice: 'Your has been made answer best'
     else
       flash[:alert] = "Unpermited action. Access denied."
-      redirect_to @question
+      redirect_to parent
     end
   end
 
-  private
+  protected
 
-  def check_authority
-    unless @answer.user_id == current_user.id
-      flash[:alert] = 'Unpermited action. Access denied.'
-      redirect_to @answer.question
-    end
+  def build_answer
+    build_resource.question = parent
   end
 
-  def set_answer
-    @answer = Answer.find(params[:id])
-  end
-
-  def load_question
-    @question = Question.find(params[:question_id])
+  def create_resource(object)
+    object.user = current_user
+    super
   end
 
   def answer_params
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,176 +1,171 @@
 require 'spec_helper'
 
 describe AnswersController do
-  describe 'POST #create' do
-    let(:question) { create(:question) }
+  # describe 'POST #create' do
+  #   let(:question) { create(:question) }
 
-    def request(type_data)
-      post :create, question_id: question, answer: attributes_for(type_data), format: :js
-    end
+  #   def request(type_data)
+  #     post :create, question_id: question, answer: attributes_for(type_data), format: :js
+  #   end
 
-    context 'user is sign in' do
-      login_user
-      context 'with valid attributes' do
-        it 'saves the new answer in the database' do
-          expect { request(:answer) }.to change(Answer, :count).by(1)
-        end
+  #   context 'user is sign in' do
+  #     login_user
+  #     context 'with valid attributes' do
+  #       it 'saves the new answer in the database' do
+  #         expect { request(:answer) }.to change(Answer, :count).by(1)
+  #       end
 
-        it 'redirects to show view of current question' do
-          request(:answer)
-          expect(response).to render_template :create
-        end
+  #       it 'redirects to show view of current question' do
+  #         request(:answer)
+  #         expect(response).to render_template :create
+  #       end
 
-        it 'saves new answer with attribute user_id' do
-          expect { request(:answer) }.to change(@user.answers, :count)
-        end
+  #       it 'saves new answer with attribute user_id' do
+  #         expect { request(:answer) }.to change(@user.answers, :count)
+  #       end
 
-        it 'saves new answer with attribute question_id' do
-          expect { request(:answer) }.to change(question.answers, :count)
-        end
+  #       it 'saves new answer with attribute question_id' do
+  #         expect { request(:answer) }.to change(question.answers, :count)
+  #       end
 
-        # it 'renders flash message about success creation' do
-        #   request(:answer)
-        #   expect(flash[:notice]).to eq 'Your answer has been successfully created.'
-        # end
-      end
+  #       # it 'renders flash message about success creation' do
+  #       #   request(:answer)
+  #       #   expect(flash[:notice]).to eq 'Your answer has been successfully created.'
+  #       # end
+  #     end
 
-      context 'with invalid attributes' do
-        it 'does not save answer to database' do
-          expect { request(:invalid_answer) }.to_not change(Answer, :count)
-        end
+  #     context 'with invalid attributes' do
+  #       it 'does not save answer to database' do
+  #         expect { request(:invalid_answer) }.to_not change(Answer, :count)
+  #       end
 
-        it 'redirectes to view show of current question' do
-          request(:invalid_answer)
-          expect(response).to  render_template :create
-        end
+  #       it 'redirectes to view show of current question' do
+  #         request(:invalid_answer)
+  #         expect(response).to  render_template :create
+  #       end
 
-        # it 'renders flash message about fail creation' do
-        #   request(:invalid_answer)
-        #   expect(flash[:alert]).to eq 'Your answer hasn`t been created. Try again.'
-        # end
-      end
-    end
+  #       # it 'renders flash message about fail creation' do
+  #       #   request(:invalid_answer)
+  #       #   expect(flash[:alert]).to eq 'Your answer hasn`t been created. Try again.'
+  #       # end
+  #     end
+  #   end
 
-    context 'user is a quest' do
-      it 'redirect to sign in page' do
-        post :create, question_id: question, answer: attributes_for(:answer)
-        expect(response).to redirect_to new_user_session_path
-      end
-    end
-  end
+  #   context 'user is a quest' do
+  #     it 'redirect to sign in page' do
+  #       post :create, question_id: question, answer: attributes_for(:answer)
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+  # end
 
-  describe 'GET #edit' do
-    let(:question) { create(:question) }
-    let(:answer) { create(:answer) }
+  # describe 'GET #edit' do
+  #   let(:question) { create(:question) }
+  #   let(:answer) { create(:answer) }
 
-    context 'user is sign in' do
-      login_user
-      before(:each) do
-         @user.answers << answer
-         get :edit, question_id: question, id: answer
-      end
+  #   context 'user is sign in' do
+  #     login_user
+  #     before(:each) do
+  #        @user.answers << answer
+  #        get :edit, question_id: question, id: answer
+  #     end
 
-      it 'assigns the requested answer to @answer' do
-        expect(assigns(:answer)).to eq answer
-      end
+  #     it 'assigns the requested answer to @answer' do
+  #       expect(assigns(:answer)).to eq answer
+  #     end
 
-      it 'renders edit view' do
-        expect(response).to render_template :edit
-      end
-    end
+  #     it 'renders edit view' do
+  #       expect(response).to render_template :edit
+  #     end
+  #   end
 
-    context 'user is a quest' do
-      it 'redirect to sign in page' do
-        get :edit, question_id: question, id: answer
-        expect(response).to redirect_to new_user_session_path
-      end
-    end
-  end
+  #   context 'user is a quest' do
+  #     it 'redirect to sign in page' do
+  #       get :edit, question_id: question, id: answer
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+  # end
 
-  describe 'PATCH #update' do
-    let(:question) { create(:question) }
-    let(:answer) { create(:answer) }
+  # describe 'PATCH #update' do
+  #   let(:question) { create(:question) }
+  #   let(:answer) { create(:answer) }
 
-    context 'user is sign in' do
-      login_user
-      before(:each) { @user.answers << answer }
+  #   context 'user is sign in' do
+  #     login_user
+  #     before(:each) { @user.answers << answer }
 
-      context 'with valid attributes' do
-        it 'assigns the requested answer to @answer' do
-          patch :update, question_id: question, id: answer, answer: attributes_for(:answer), format: :js
-          expect(assigns(:answer)).to eq answer
-        end
+  #     context 'with valid attributes' do
+  #       it 'assigns the requested answer to @answer' do
+  #         patch :update, question_id: question, id: answer, answer: attributes_for(:answer), format: :js
+  #         expect(assigns(:answer)).to eq answer
+  #       end
 
-        it 'changes answer attributes' do
-          patch :update, question_id: question, id: answer, answer: { text: "some text"*10 }, format: :js
-          answer.reload
-          expect(answer.text).to eq "some text"*10
-        end
+  #       it 'changes answer attributes' do
+  #         patch :update, question_id: question, id: answer, answer: { text: "some text"*10 }, format: :js
+  #         answer.reload
+  #         expect(answer.text).to eq "some text"*10
+  #       end
 
-        it 'redirects to view show question page' do
-          patch :update, question_id: question, id: answer, answer: attributes_for(:answer), format: :js
-          expect(response).to render_template :update
-        end
+  #       it 'render update.js' do
+  #         patch :update, question_id: question, id: answer, answer: attributes_for(:answer), format: :js
+  #         expect(response).to render_template :update
+  #       end
 
-        #  it 'renders flash message about success updating' do
-        #   patch :update, question_id: question, id: answer, answer: { text: "some text"*10 }
-        #   expect(flash[:notice]).to eq 'Answer has been successfully updated.'
-        # end
-      end
+  #       #  it 'renders flash message about success updating' do
+  #       #   patch :update, question_id: question, id: answer, answer: { text: "some text"*10 }
+  #       #   expect(flash[:notice]).to eq 'Answer has been successfully updated.'
+  #       # end
+  #     end
 
-      context 'with invalid attributes' do
+  #     context 'with invalid attributes' do
 
-        it 'does not change answer attributes' do
-          patch :update, question_id: question, id: answer, answer: { text: "some text" }
-          answer.reload
-          expect(answer.text).to eq answer.text
-        end
+  #       it 'does not change answer attributes' do
+  #         patch :update, question_id: question, id: answer, answer: { text: "some text" }
+  #         answer.reload
+  #         expect(answer.text).to eq answer.text
+  #       end
 
-        it 're-renders edit view' do
-          patch :update, question_id: question, id: answer, answer: { text: "some text" }
-          expect(response).to render_template :edit
-        end
+  #       it 're-renders update view' do
+  #         patch :update, question_id: question, id: answer, answer: { text: "some text" }
+  #         expect(response).to render_template :update
+  #       end
+  #     end
+  #   end
 
-        it 'renders flash message about fail updating' do
-          patch :update, question_id: question, id: answer, answer: { text: "some text" }
-          expect(flash[:alert]).to eq 'Answer hasn`t been updated. Try again.'
-        end
-      end
-    end
+  #   context 'user is a quest' do
+  #     it 'redirect to sign in page' do
+  #       patch :update, question_id: question, id: answer
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+  # end
 
-    context 'user is a quest' do
-      it 'redirect to sign in page' do
-        patch :update, question_id: question, id: answer
-        expect(response).to redirect_to new_user_session_path
-      end
-    end
-  end
+  # describe 'DELETE #destroy' do
 
-  describe 'DELETE #destroy' do
+  #   let(:question) { create(:question) }
+  #   let(:answer) { create(:answer) }
 
-    let(:question) { create(:question) }
-    let(:answer) { create(:answer) }
+  #   context 'user is sign in' do
+  #     login_user
+  #     # before(:each) { @user.answers << answer }
 
-    context 'user is sign in' do
-      login_user
-      before(:each) { @user.answers << answer }
+  #     it 'deletes answer' do
+  #       answer
+  #       expect { delete :destroy, question_id: question, id: answer, format: :js }.to change(Answer, :count).by(-1)
+  #     end
 
-      it 'deletes answer' do
-        answer
-        expect { delete :destroy, question_id: question, id: answer, format: :js }.to change(Answer, :count).by(-1)
-      end
+  #     it 'render destroy.js.erb' do
+  #       delete :destroy, question_id: question, id: answer, format: :js
+  #       expect(response).to render_template :destroy
+  #     end
+  #   end
 
-      it 'render destroy.js.erb' do
-        delete :destroy, question_id: question, id: answer, format: :js
-        expect(response).to render_template :destroy
-      end
-    end
-
-    context 'user is a quest' do
-      it 'redirects to sign in page' do
-        delete :destroy, question_id: question, id: answer
-        expect(response).to redirect_to new_user_session_path
-      end
-    end
-  end
+  #   context 'user is a quest' do
+  #     it 'redirects to sign in page' do
+  #       delete :destroy, question_id: question, id: answer
+  #       expect(response).to redirect_to new_user_session_path
+  #     end
+  #   end
+  # end
 end
